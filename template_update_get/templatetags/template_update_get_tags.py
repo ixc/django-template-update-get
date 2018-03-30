86398de70153264195e1fc7da66538de5d6497dd
Mark Finger <markfinger@gmail.com>
2018-03-30 15:48:41 +1100

    Python 3 compatibility

--- template_update_get/templatetags/template_update_get_tags.py
+++ template_update_get/templatetags/template_update_get_tags.py
@@ -30,7 +30,7 @@ def do_update_get(parser, token):
 
 def _chunks(l, n):
     """Yield successive n-sized chunks from l."""
-    for i in xrange(0, len(l), n):
+    for i in range(0, len(l), n):
         yield l[i:i+n]
 
 
